d21f0277e79f79195c25122cd5c77a765edb8a73
James <jm0393@icloud.com>
2017-01-30 06:28:50 -0600

    Got list to show in options-box but w/errors

--- js/app.js
+++ js/app.js
@@ -14,148 +14,189 @@ function initMap() {
     });
 
     var largeInfoWindow = new google.maps.InfoWindow();
+
     placeMarkers();
+
     ko.applyBindings(new ViewModel());
 
 }
 
+var currentPlaces = [{
+geometry: {
+    lat: 33.207634,
+    lng: -87.550510
+},
+properties: 'Bryant-Denny Stadium'
+}, {
+geometry: {
+    lat: 33.209875,
+    lng: -87.546295
+},
+properties: 'Denny Chimes'
+}, {
+geometry: {
+    lat: 33.213905,
+    lng: -87.530016
+},
+properties: 'Rhoads Stadium'
+}, {
+geometry: {
+    lat: 33.210787,
+    lng: -87.532373
+},
+properties: 'Soccer Stadium'
+}, {
+geometry: {
+    lat: 33.204686,
+    lng: -87.538614
+},
+properties: 'Sewell-Thomas Stadium'
+}, {
+geometry: {
+    lat: 33.207634,
+    lng: -87.550510
+},
+properties: 'Kinshasa'
+}, {
+geometry: {
+    lat: 33.202945,
+    lng: -87.539768
+},
+properties: 'Sydney'
+}, {
+geometry: {
+    lat: 33.206623,
+    lng: -87.539650
+},
+properties: 'Bryant Museum'
+}];
 
-    var currentPlaces = [{
-        geometry: {
-            lat: 33.207634,
-            lng: -87.550510
-        },
-        properties: 'Bryant-Denny Stadium'
-    }, {
-        geometry: {
-            lat: 33.209875,
-            lng: -87.546295
-        },
-        properties: 'Denny Chimes'
-    }, {
-        geometry: {
-            lat: 33.213905,
-            lng: -87.530016
-        },
-        properties: 'Rhoads Stadium'
-    }, {
-        geometry: {
-            lat: 33.210787,
-            lng: -87.532373
-        },
-        properties: 'Soccer Stadium'
-    }, {
-        geometry: {
-            lat: 33.204686,
-            lng: -87.538614
-        },
-        properties: 'Sewell-Thomas Stadium'
-    }, {
-        geometry: {
-            lat: 33.207634,
-            lng: -87.550510
-        },
-        properties: 'Kinshasa'
-    }, {
-        geometry: {
-            lat: 33.202945,
-            lng: -87.539768
-        },
-        properties: 'Sydney'
-    }, {
-        geometry: {
-            lat: 33.206623,
-            lng: -87.539650
-        },
-        properties: 'Bryant Museum'
-    }];
 
+var placeMarkers = function(){
 
-    var placeMarkers = function(){
+var bounds = new google.maps.LatLngBounds();
 
-        var bounds = new google.maps.LatLngBounds();
+for (var i = 0; i < currentPlaces.length; i++) {
 
-        for (var i = 0; i < currentPlaces.length; i++) {
+    var position = currentPlaces[i].geometry;
+    var title = currentPlaces[i].properties;
 
-            var position = currentPlaces[i].geometry;
-            var title = currentPlaces[i].properties;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: google.maps.Animation.BOUNCE,
+        id: i
+    });
 
-            var marker = new google.maps.Marker({
-                map: map,
-                position: position,
-                title: title,
-                animation: google.maps.Animation.BOUNCE,
-                id: i
-            });
-
-            markers.push(marker);
-            
-            marker.addListener('click', function() {
-                populateInfoWindow(this, largeInfoWindow);
-                toggleBounce();
-
-            });
-
-            bounds.extend(markers[i].position);
-            // currentPlaces[i].marker = marker;
-            // marker.addListener('click', toggleBounce);
-        }
-
-        map.fitBounds(bounds);
+    markers.push(marker);
     
+    marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfoWindow);
+        toggleBounce();
 
-        function toggleBounce() {
-            for (var i = 0; i < markers.length; i ++)
+    });
 
-                var currentMarker = null;
+    bounds.extend(markers[i].position);
+    // currentPlaces[i].marker = marker;
+    // marker.addListener('click', toggleBounce);
+    function toggleBounce() {
+    for (var i = 0; i < markers.length; i ++)
 
-                if (currentMarker) {
-                    currentMarker.setAnimation(null);
-                } else {
+        var currentMarker = null;
 
-                    marker.setAnimation(google.maps.Animation.BOUNCE);
-                }
+        if (currentMarker) {
+            currentMarker.setAnimation(null);
+        } else {
+
+            marker.setAnimation(google.maps.Animation.BOUNCE);
         }
+}
+}
+
+map.fitBounds(bounds);
 
 
-        function populateInfoWindow (marker, infowindow) {
+function populateInfoWindow (marker, infowindow) {
 
+            // Check to make sure the infowindow is not already opened on this marker.
+if (infowindow.marker != marker) {
+  // Clear the infowindow content to give the streetview time to load.
+  infowindow.setContent('');
+  infowindow.marker = marker;
+  // Make sure the marker property is cleared if the infowindow is closed.
+  infowindow.addListener('closeclick', function() {
+    infowindow.marker = null;
+  });
+  var streetViewService = new google.maps.StreetViewService();
+  var radius = 50;
+  // In case the status is OK, which means the pano was found, compute the
+  // position of the streetview image, then calculate the heading, then get a
+  // panorama from that and set the options
+  function getStreetView(data, status) {
+    if (status == google.maps.StreetViewStatus.OK) {
+      var nearStreetViewLocation = data.location.latLng;
+      var heading = google.maps.geometry.spherical.computeHeading(
+        nearStreetViewLocation, marker.position);
+        infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+        var panoramaOptions = {
+          position: nearStreetViewLocation,
+          pov: {
+            heading: heading,
+            pitch: 30
+          }
         };
-
-
+      var panorama = new google.maps.StreetViewPanorama(
+        document.getElementById('pano'), panoramaOptions);
+    } else {
+      infowindow.setContent('<div>' + marker.title + '</div>' +
+        '<div>No Street View Found</div>');
     }
+  }
+  // Use streetview service to get the closest streetview image within
+  // 50 meters of the markers position
+  streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+  // Open the infowindow on the correct marker.
+  infowindow.open(map, marker);
+}
+
+};
 
 
-    var Places = function(data) {
-        this.place = ko.observable(data.geometry);
-        this.title = ko.observable(data.properties);
+}
+
+
+var Places = function(data) {
+    var self = this;
+    self.place = ko.observable(data.geometry);
+    self.title = ko.observable(data.properties);
+};
+
+var ViewModel = function() {
+
+    var self = this;
+
+    this.placesList = ko.observableArray([]);
+    self.markers = ko.observableArray([]);
+
+    currentPlaces.forEach(function(item) {
+        self.placesList.push(new Places(item));
+    });
+
+    this.currentPlace = ko.observable(this.placesList()[0]);
+
+    this.setPlace = function(clickedPlaces){
+        self.currentPlace(clickedPlaces);
     };
 
-    var ViewModel = function() {
+    self.currentPlace = function() {
+        populateInfoWindow(this, infowindow);
+    };
 
-        var self = this;
-
-        this.placesList = ko.observableArray([]);
-        // self.markers = ko.observableArray([]);
-
-        currentPlaces.forEach(function(item) {
-            self.placesList.push(new Places(item));
-        });
-
-        this.currentPlace = ko.observable(this.placesList());
-
-        this.setPlace = function(clickedPlaces){
-        	self.currentPlace(clickedPlaces);
-        };
-
-        self.currentPlace = function() {
-            populateInfoWindow(this, infowindow);
-        };
-
-        this.setMarker = function(clickedPlaces) {
-            sel.currentPlace(clickedPlaces);
-        };
+    this.setMarker = function(clickedPlaces) {
+        self.currentPlace(clickedPlaces);
+    };
 
 
-        
-    }
+    
+}
